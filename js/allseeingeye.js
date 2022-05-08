0faa9a2beb6a829ec22248b54faf5223df0c46a9
Krugou <59641229+Krugou@users.noreply.github.com>
2022-05-08 11:15:30 +0300

    Update allseeingeye.js

--- js/allseeingeye.js
+++ js/allseeingeye.js
@@ -1,7 +1,7 @@
 const tags = "./json/tags.json";
-
-let website = (document.body.innerHTML = randomize());
-
+let content;
+let content2;
+randomize();
 function randomize() {
   fetch(tags)
     .then((response) => {
@@ -11,7 +11,20 @@ function randomize() {
         throw "HTTP ERROR";
       }
     })
-    .then((jsonData) => {
-      tags.
+    .then((tags) => {
+      for (let i = 0; i < tags.elementsmain.length; i++) {
+        content2 += tags.elementsmain[i].tagStart
+        content2 += tags.elementsmain[i].name
+        content2 += tags.elementsmain[i].tagEnd;
+        console.log(tags.elementsmain[i].tagStart);
+        console.log(content2);
+      }
+      content =
+        tags.elementsmain[0].tagStart +
+        tags.elementsmain[0].name+" " +
+        content2 +
+        tags.elementsmain[0].tagEnd;
+      console.log(content);
+      let website = (document.body.innerHTML = content);
     });
 }
